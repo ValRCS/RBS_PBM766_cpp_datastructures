bf2125f1a7e9f491c66df7fbeae0c4cd1e454ee1
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2024-10-24 08:45:05 +0000

    basic try catch

--- exceptions.cpp
+++ exceptions.cpp
@@ -10,12 +10,22 @@ int main() {
     int a = 10;
     int b = 0;
 
-    //if b is zero, we will throw an exception
-    if (b == 0) {
-        throw "Division by zero";
+//so if make a try block I need have at least one catch block to catch the exception
+    try {
+        //if b is zero, we will throw an exception
+        if (b == 0) {
+            throw "Division by zero"; //we throw a c string here
+        }
+        //this means we survived the division by zero
+        //we are guaranteed that b is not zero
+        cout << a / b << endl;
+    } catch (const char* msg) {
+        //this is the catch block that will catch the exception
+        //we can do whatever else we would like to do here when b is zero
+        //msg is the message that was thrown
+        cout << "Exception caught: " << msg << endl;
     }
+  
 
-    cout << a / b << endl;
-
-    return 0;
+    return EXIT_SUCCESS;
 }
