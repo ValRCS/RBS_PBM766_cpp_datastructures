e24a114767492242b66005e2b87b5e2ac67972d4
Valdis Saulespurens <38938337+ValRCS@users.noreply.github.com>
2024-11-14 10:47:48 +0000

    stl examples

--- stl.cpp
+++ stl.cpp
@@ -25,6 +25,26 @@ void print_vector(const vector<T>& v) {
     cout << endl;
 }
 
+//let's make a generic function to print a map
+template <typename K, typename V>
+void print_map(const map<K, V>& m) {
+    //we use iterators to iterate over the map
+    for (auto it = m.begin(); it != m.end(); it++) {
+        cout << it->first << " : " << it->second << endl; //first is the key, second is the value
+    }
+    cout << endl;
+}
+
+//let's make a generic function to print an unordered map
+template <typename K, typename V>
+void print_map(const unordered_map<K, V>& m) {
+    //we use iterators to iterate over the map
+    for (auto it = m.begin(); it != m.end(); it++) {
+        cout << it->first << " : " << it->second << endl; //first is the key, second is the value
+    }
+    cout << endl;
+}
+
 int main(int argc, char* argv[]) {
     //let's create a vector of integers
     vector<int> v = {1, 2, 3, 4, 5};
@@ -77,5 +97,52 @@ int main(int argc, char* argv[]) {
 
     //let's print the vector again
     print_vector(v);
+
+    //let's shuffle the vector
+    random_shuffle(v.begin(), v.end()); //this is O(n) operation
+
+    //let's print the vector again
+    print_vector(v);
+
+    //let's create a map of char to int
+    map<char, int> m; //again this is ORDERED! so O(log n) operations
+
+    //let's create a map from characters in a string to their unicode values
+    string s = "Valdis";
+    for (size_t i = 0; i < s.size(); i++) {
+        m[s[i]] = s[i]; //left side assignent is O(log n) operation, s[i] is O(1) operation of course    
+    }
+    //let's print the map
+    print_map(m);
+
+    //now let's create an unordered map that maps utf8 characters to their unicode values
+    //we can not use char we have to use the utf8 string
+    // unordered_map<string, int> um; //this is UNORDERED! so O(1) operations often we want this!
+    //I will go back to ordered map for a moment
+    map<string,int> um; //this is ORDERED! so O(log n) operations
+
+    string utf8_string = "Spožā Rīga"; //this is a utf8 string
+    // for (size_t i = 0; i < utf8_string.size(); i++) {
+    //     //print what we are processing
+    //     cout << "Processing" << utf8_string.substr(i, 1) << endl;
+    //     string utf8_char = utf8_string.substr(i, 1); //this is a utf8 character
+    //     um[utf8_char] = utf8_char[0]; //left side assignent is O(1) operation, utf8_char[0] is O(1) operation of course
+    // }
+    //above is not completely correct because utf8 characters can be more than one byte!
+
+    // //let's use iterators to go over utf8 string
+    // for (auto it = utf8_string.begin(); it != utf8_string.end(); it++) {
+    //     //print what we are processing
+    //     cout << "Processing" << utf8_string.substr(it - utf8_string.begin(), 1) << endl;
+    //     string utf8_char = utf8_string.substr(it - utf8_string.begin(), 1); //this is a utf8 character
+    //     um[utf8_char] = utf8_char[0]; //left side assignent is O(1) operation, utf8_char[0] is O(1) operation of course
+    // }
+
+    //let's iterate properly over utf8 string, we could have multiple bytes for one character
+
+
+    //let's print the unordered map
+    print_map(um); //it works now since we made a function that works with unordered_map
+
 
 }
